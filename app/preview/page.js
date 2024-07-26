e51eaedda32a3600b492dc1287b9bde3588b824a
reggeditt <tkodjosarso@gmail.com>
2024-07-26 08:03:18 +0000

    add profile page basic outline

---
+++ app/preview/page.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const PreviewPage = () => {
+  return (
+    <div className='relative bg-[#fafafa]'>
+      <header className='p-5 bg-[#633CFF]'>
+        <nav className='bg-white flex justify-between items-center pt-2 pb-2 pl-5 pr-5 rounded-xl'>
+          <button className='bg-white text-[#633CFF] border border-[#633CFF] px-4 py-2 rounded-lg'>Back to Editor</button>
+          <button className='bg-[#633CFF] text-white px-4 py-2 rounded-lg'>Share Link</button>
+        </nav>
+      </header>
+      <main className='relative'>
+        <div className='banner bg-[#633CFF] rounded-b-2xl w-full h-[200px]'/>
+        <div 
+          className={`
+            absolute w-[250px] h-[400px] bg-white top-20 left-[calc((100%-250px)/2)] rounded-2xl shadow-lg
+            flex flex-col justify-center items-center
+          `}
+        >
+          profile render goes here
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default PreviewPage
+ 
